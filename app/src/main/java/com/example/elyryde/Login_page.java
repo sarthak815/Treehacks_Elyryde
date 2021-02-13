86fd863cffed032b7e1d2f9c4a20d0d6de8d353a
Sarthak Shastri <55555570+sarthak815@users.noreply.github.com>
2021-02-13 19:06:53 +0530

    Register page complete

--- app/src/main/java/com/example/elyryde/Login_page.java
+++ app/src/main/java/com/example/elyryde/Login_page.java
@@ -3,12 +3,37 @@ package com.example.elyryde;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login_page extends AppCompatActivity {
+
+    Button register;
+    EditText userid, password;
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+        mAuth = FirebaseAuth.getInstance();
+        userid = (EditText)findViewById(R.id.username_login);
+        password = (EditText)findViewById(R.id.password_login);
+        register = (Button)findViewById(R.id.loginbtn);
+    }
+
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+
+    }
+
+    private void updateUI(FirebaseUser currentUser) {
     }
 }
---
+++ app/src/main/java/com/example/elyryde/Register.java
@@ -0,0 +1,113 @@
+package com.example.elyryde;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
+public class Register extends AppCompatActivity {
+    Button register;
+    EditText username, email, password, name;
+    ProgressDialog progressDialog;
+    private FirebaseAuth mAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mAuth = FirebaseAuth.getInstance();
+        setContentView(R.layout.activity_register);
+        register = (Button) findViewById(R.id.registerbtn);
+        username = (EditText) findViewById(R.id.username_register);
+        email = (EditText) findViewById(R.id.emailid_register);
+        password = (EditText) findViewById(R.id.password_register);
+        name = (EditText) findViewById(R.id.name_register);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email1 = email.getText().toString().trim();
+                String password1 = password.getText().toString().trim();
+
+                if(!Patterns.EMAIL_ADDRESS.matcher(email1).matches()){
+                    email1.setError("Invalid Email");
+                    email1.setFocusable(true);
+                }
+                else if(password.length()<6){
+                    password.setError("Password length must be greater than 6");
+                    password.setFocusable(true);
+                }
+                else{
+                    registerUser(email, password1);
+                }
+            }
+        });
+    }
+
+    private void registerUser(String email, String password1) {
+
+        progressDialog.show();
+
+        mAuth.createUserWithEmailAndPassword(email, password1)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            progressDialog.dismiss();
+
+                            // Sign in success, update UI with the signed-in user's informatio
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                            String email = user.getEmail();
+                            String uid = user.getUid();
+
+                            HashMap<Object, String> hashMap = new HashMap<>();
+                            hashMap.put("email", email);
+                            hashMap.put("uid", uid);
+                            hashMap.put("name", email);
+                            hashMap.put("phone", email);
+                            hashMap.put("image", email);
+                            //getting database
+
+                            FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+                            DatabaseReference reference = database.getReference("Users");
+
+                            reference.child(uid).setValue(hashMap);
+
+                            Toast.makeText(Register.this, "Registering"+ user.getEmail(), Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(Register.this, feed.class));
+                            finish();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(Register.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+                        // ...
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                progressDialog.show();
+                Toast.makeText(Register.this, ""+ e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
