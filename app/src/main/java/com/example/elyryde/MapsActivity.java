1afa9ff0c2548d911390c6ff11df2dd6d4235632
Aman Priyanshu <aman.priyanshu@learner.manipal.edu>
2021-02-14 19:17:25 +0530

    firebase finals

--- app/src/main/java/com/example/elyryde/MapsActivity.java
+++ app/src/main/java/com/example/elyryde/MapsActivity.java
@@ -1,31 +1,48 @@
 package com.example.elyryde;
 
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private ArrayList<LatLng> locationArrayList;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("charging_stations");
+    FirebaseUser user;
+
 
     LatLng sydney = new LatLng(-34, 151);
-    LatLng TamWorth = new LatLng(-31.083332, 150.916672);
+    //LatLng TamWorth = new LatLng(-31.083332, 150.916672);
     LatLng NewCastle = new LatLng(-32.916668, 151.750000);
     LatLng Brisbane = new LatLng(-27.470125, 153.021072);
 
@@ -37,18 +54,80 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
         locationArrayList = new ArrayList<>();
 
-        // on below line we are adding our
-        // locations in our array list.
-        locationArrayList.add(sydney);
-        locationArrayList.add(TamWorth);
-        locationArrayList.add(NewCastle);
-        locationArrayList.add(Brisbane);
+        DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference tripsRef = rootRef.child("charging_stations");
+        ValueEventListener valueEventListener = new ValueEventListener()
+        {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                List<Double> list_lat = new ArrayList<>();
+                List<Double> list_long = new ArrayList<>();
+                List<String> city = new ArrayList<>();
+                for(DataSnapshot ds : dataSnapshot.getChildren()) {
+                    int i = 0;
+                    Double latitude = ds.child("latitude").getValue(Double.class);
+                    Double longitude = ds.child("longitude").getValue(Double.class);
+                    String city1 = ds.child("City").getValue(String.class);
+                    Log.d("TAG", latitude + " / " + longitude + " / " + city1);
+                    list_lat.add(latitude);
+                    list_long.add(longitude);
+                    city.add(city1);
+                    LatLng x = new LatLng(list_lat.get(i), list_long.get(i));
+                    locationArrayList.add(x);
+                    mMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title("Marker"));
+
+                    // below lin is use to zoom our camera on map.
+                    mMap.animateCamera(CameraUpdateFactory.zoomTo(1.0f));
+
+                    // below line is use to move our camera to the specific location.
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(latitude,longitude)));
+
+                    mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+                        @Override
+                        public boolean onMarkerClick(Marker marker) {
+                            BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(
+                                    MapsActivity.this, R.style.BottomSheetDialogTheme
+                            );
+                            View botttomSheetView = LayoutInflater.from(getApplicationContext())
+                                    .inflate(
+                                            R.layout.layout_bottom,
+                                            (LinearLayout) findViewById(R.id.bottomSheetContainer)
+                                    );
+                            botttomSheetView.findViewById(R.id.button_confirm);
+                            botttomSheetView.setOnClickListener(v -> {
+                                Toast.makeText(MapsActivity.this, "Booking Confirmed", Toast.LENGTH_SHORT).show();
+
+                            });
+                            bottomSheetDialog.setContentView(botttomSheetView);
+                            bottomSheetDialog.show();
+
+                            return true;
+                        }
+
+                    });
+
+                    i++;
+
+
+                }
+                Log.d("TAG", list_lat.get(0) + " / " + list_long.get(0) + " / ");
+
+
+
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {}
+        };
+        tripsRef.addListenerForSingleValueEvent(valueEventListener);
 
 
     }
+
+
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
@@ -57,6 +136,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // we will be displaying all our markers.
         // for adding markers we are running for loop and
         // inside that we are drawing marker on our map.
+
         for (int i = 0; i < locationArrayList.size(); i++) {
 
             // below line is use to add marker to each location of our array list.
